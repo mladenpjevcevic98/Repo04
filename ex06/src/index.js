d16654c0639c06bddd4c860d1d8a3539023290e2
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-17 19:35:35 +0200

    ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,19 @@
+function main(numOne, numTwo)
+{
+    if(numTwo===0)
+        {
+            myQuotient = 'Divide by 0. Cannot divide by zero.'
+        }
+    else 
+        {
+            myQuotient = numOne / numTwo;
+        }
+
+        return myQuotient;
+}
+
+console.log(main(5.0, 2.0));
+console.log(main(16.5, 5.5));
+console.log(main(102.0, 25.5));
+
+module.exports = main;
