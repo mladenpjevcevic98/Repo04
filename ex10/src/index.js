daf16932e7b68fa28a4884ca3c69ca76bed0568c
mladenpjevcevic98 <pjevcevicm237@gmail.com>
2021-09-17 19:36:59 +0200

    ex10

---
+++ ex10/src/index.js
@@ -0,0 +1,12 @@
+function main()
+{
+    var myFirstName = "Mladen";
+    var myLastName = "Pjevcevic";
+    var myHomeTown = "Visegrad";
+
+    return{myFirstName, myLastName, myHomeTown};
+}
+
+console.log(main());
+
+module.exports = main;
